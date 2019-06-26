ce6363b035f07125f8e86f9c04780e9220fdbfdd
TheGroundZero <2406013+TheGroundZero@users.noreply.github.com>
2019-06-26 09:41:42 +0000

    Run on port 80

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-server_address = ("", 8000)
+server_address = ("", 80)
 
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 httpd.serve_forever()

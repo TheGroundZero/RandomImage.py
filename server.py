a772b56ed36a980a95d5f3ff7833ef685ab994d2
TheGroundZero <2406013+TheGroundZero@users.noreply.github.com>
2019-06-26 12:26:46 +0200

    Capture KeyboardInterrupt

--- server.py
+++ server.py
@@ -2,7 +2,14 @@
 
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
+server = HTTPServer
+handler = CGIHTTPRequestHandler
 server_address = ("", 80)
+handler.cgi_directories = ["/cgi-bin"]
 
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
+httpd = HTTPServer(server_address, handler)
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    httpd.shutdown()

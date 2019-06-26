52bdb798fdc8fb1830768ffea390c16fd00e9eb5
TheGroundZero <2406013+TheGroundZero@users.noreply.github.com>
2019-06-26 14:00:00 +0200

    Added no-cache headers

--- cgi-bin/img.py
+++ cgi-bin/img.py
@@ -13,15 +13,12 @@ ext2conttype = {
 }
 
 
-def main():
-    directory = "./img/"
-    img = random_file(directory)
-    display_image(directory + img)
-
-
 def display_image(src):
     sys.stdout.write("Content-Type: {}\n".format(content_type(src)))
     sys.stdout.write("Content-Length: " + str(os.stat(src).st_size) + "\n")
+    sys.stdout.write("Cache-Control: no-cache, no-store, must-revalidate\n")
+    sys.stdout.write("Pragma: no-cache\n")
+    sys.stdout.write("Expires: 0\n")
     sys.stdout.write("\n")
     sys.stdout.flush()
     sys.stdout.buffer.write(open(src, "rb").read())
@@ -43,6 +40,12 @@ def is_image(file):
 def random_file(dir):
     files = [f for f in listdir(dir) if is_image(f)]
     return choice(files)
+
+
+def main():
+    directory = "./img/"
+    img = random_file(directory)
+    display_image(directory + img)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -2,14 +2,30 @@
 
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-server = HTTPServer
-handler = CGIHTTPRequestHandler
-server_address = ("", 80)
-handler.cgi_directories = ["/cgi-bin"]
 
-httpd = HTTPServer(server_address, handler)
+class MyCGIHTTPRequestHandler(CGIHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()
+        CGIHTTPRequestHandler.end_headers(self)
 
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    httpd.shutdown()
+    def send_my_headers(self):
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+
+
+def main():
+    handler = MyCGIHTTPRequestHandler
+    handler.cgi_directories = ["/cgi-bin"]
+
+    httpd = HTTPServer(("", 80), handler)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.shutdown()
+
+
+if __name__ == "__main__":
+    main()
